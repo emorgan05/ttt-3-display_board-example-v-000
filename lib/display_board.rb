db3f8e1ef0c1d295dc0fbd14d1a5dcac4b9b9f4d
Emily Morgan <emorgan05@gmail.com>
2017-09-01 19:11:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  cell = "   "
+  cell = "  "
   vertical = "|"
-  horizontal = "___________"
+  horizontal = "-----------"
   row = "#{cell} #{vertical} #{cell} #{vertical} #{cell}"
   puts row
   puts horizontal
